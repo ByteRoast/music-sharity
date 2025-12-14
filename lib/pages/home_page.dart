f11d985f1ef4e2a9b76e81555aab7ab05b594ea8
Sik <contact@sikelio.wtf>
2025-12-14 20:25:20 +0100

    feat(docs): added licence gpl v3

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,9 +1,25 @@
+/*
+ * Music Sharity - Convert music links between streaming platforms
+ * Copyright (C) 2025 Sikelio (Byte Roast)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
 import 'package:flutter/material.dart';
 import 'about_page.dart';
 import 'conversion_page.dart';
 import '../models/music_link.dart';
 import '../utils/link_validator.dart';
-import '../models/music_link.dart';
 
 class HomePage extends StatefulWidget {
   final String? initialLink;
